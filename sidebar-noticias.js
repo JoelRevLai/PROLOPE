e6929611511a1e78120e01378591cf7982093a33
Joel Reverter <joelrevertergmail.com>
2026-03-15 11:36:32 +0100

    0.26

--- sidebar-noticias.js
+++ sidebar-noticias.js
@@ -4,15 +4,19 @@
   var sidebar = document.querySelector('.sidebar-news-list');
   if (!sidebar || typeof news === 'undefined') return;
 
+  function esc(s) {
+    return String(s).replace(/&/g,'&amp;').replace(/</g,'&lt;').replace(/>/g,'&gt;').replace(/"/g,'&quot;').replace(/'/g,'&#39;');
+  }
+
   var recent = news.filter(function (n) { return n.url !== currentPage; }).slice(0, 5);
 
   sidebar.innerHTML = recent.map(function (n) {
     return '<li class="sidebar-news-item">' +
-      '<a class="sidebar-news-link" href="' + n.url + '">' +
-        '<img class="sidebar-news-thumb" src="' + n.img + '" alt="" />' +
+      '<a class="sidebar-news-link" href="' + esc(n.url) + '">' +
+        '<img class="sidebar-news-thumb" src="' + esc(n.img) + '" alt="" />' +
         '<div class="sidebar-news-text">' +
-          '<div class="sidebar-news-date">' + n.date + '</div>' +
-          '<div class="sidebar-news-title">' + n.title + '</div>' +
+          '<div class="sidebar-news-date">' + esc(n.date) + '</div>' +
+          '<div class="sidebar-news-title">' + esc(n.title) + '</div>' +
         '</div>' +
       '</a>' +
     '</li>';
